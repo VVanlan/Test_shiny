337e44987b7ba41fec2fcbe6f71b088ad6394e2c
VVanlan <virginie.vanlandeghem@mffp.gouv.qc.ca>
2023-02-28 16:22:12 -0500

    Change pour data local

--- app.R
+++ app.R
@@ -35,7 +35,7 @@ server <- function(input, output) {
 
   output$main_plot <- renderPlot({
     hist(
-      faithful$eruptions,
+      faithful2$eruptions,
       probability = TRUE,
       breaks = as.numeric(input$n_breaks),
       xlab = "Duration (minutes)",
@@ -43,11 +43,11 @@ server <- function(input, output) {
     )
 
     if (input$individual_obs) {
-      rug(faithful$eruptions)
+      rug(faithful2$eruptions)
     }
 
     if (input$density) {
-      dens <- density(faithful$eruptions, adjust = input$bw_adjust)
+      dens <- density(faithful2$eruptions, adjust = input$bw_adjust)
       lines(dens, col = "blue")
     }
   })
